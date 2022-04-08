07cfa48d8c0ccde1ed90ca0d412745e4355638a8
nineey <56111891+nineey@users.noreply.github.com>
2022-04-08 22:45:42 +0200

    Add NextPage datatype

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { NextPage } from 'next';
 import {Background} from '../components/ui/background';
 import {Title} from '../components/ui/title';
 
-const Home = () => {
+const Home: NextPage = () => {
   return (
     <Background>
       <Title>Hello Next.js</Title>
