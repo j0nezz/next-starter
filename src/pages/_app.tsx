9a832ae9c6100f22d00afbb0cbc93a4817d821ed
Jonas Brunner <jonas.brunner@outlook.com>
2022-06-27 13:13:00 +0200

    remove duplicate theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,7 @@
-import {makeTheme} from 'axelra-react-utilities';
 import {NextPage} from 'next';
 import {DefaultSeo} from 'next-seo';
 import {AppProps} from 'next/app';
 import {ReactElement, ReactNode} from 'react';
-import {ThemeProvider} from 'styled-components';
 import SEO from '../lib/next-seo.config';
 import {GlobalStyle} from '../theme/theme';
 
@@ -19,11 +17,11 @@ const App = ({Component, pageProps}: AppPropsWithLayout) => {
   const getLayout = Component.getLayout ?? (page => page);
 
   return (
-    <ThemeProvider theme={makeTheme({})}>
+    <>
       <DefaultSeo {...SEO} />
       <GlobalStyle />
       {getLayout(<Component {...pageProps} />)}
-    </ThemeProvider>
+    </>
   );
 };
 
